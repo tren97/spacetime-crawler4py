0979ee545c5d803a11344b33087f603112bb108d
tren97 <lexjreyty@gmail.com>
2020-02-11 15:04:59 -0800

    Added imports

--- scraper.py
+++ scraper.py
@@ -1,13 +1,19 @@
 import re
 from urllib.parse import urlparse
+import requests as req
+import bs4
+import lxml
+
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
     return [link for link in links if is_valid(link)]
 
+
 def extract_next_links(url, resp):
     # Implementation requred.
     return list()
+
 
 def is_valid(url):
     try:
