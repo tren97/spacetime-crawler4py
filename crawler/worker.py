81ac9533825831202828d377628fc3147fe3cd94
Matthew Mueller <mrm.mue@gmail.com>
2020-02-17 14:54:19 -0800

    things

--- crawler/worker.py
+++ crawler/worker.py
@@ -48,7 +48,7 @@ class Worker(Thread):
                 self.logger.info(
                     f"Downloaded {tbd_url}, status <{resp.status}>, "
                     f"using cache {self.config.cache_server}.")
-                scraped_urls = scraper(tbd_url, resp, seen_urls, disallowed_urls, words, highWordUrl, highWordNum)
+                scraped_urls = scraper(tbd_url, resp, seen_urls, disallowed_urls, words, icsUrls, highWordUrl, highWordNum)
                 for scraped_url in scraped_urls:
                     self.frontier.add_url(scraped_url)
                 self.frontier.mark_url_complete(tbd_url)
